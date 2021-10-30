610bc4a5a4e78054adf4f3b62c1863782ca3fefd
Arno Cellarier <mrfeyurikenpro@gmail.com>
2021-10-30 14:33:40 +0200

    add hashname

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,25 @@
 const {src, dest, series, parallel} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const htmlmin = require('gulp-htmlmin');
-const minifyInline = require('gulp-minify-inline');
-const clean = require('gulp-clean');
 const inlinesource = require('gulp-inline-source-html');
 const del = require('del');
+const terser = require('gulp-terser');
+const replace = require('gulp-replace');
 
 function clearDist() {
     return del('dist');
 }
 
 function convertSass() {
-    return src('src/style.scss')
+    return src('src/styles/*.scss')
         .pipe(sass())
-        .pipe(dest('tmp'));
+        .pipe(dest('dist/styles'));
 }
 
-function clearTmp() {
-    return del('tmp');
+function minifyMjs() {
+    return src('src/scripts/*.mjs')
+        .pipe(terser())
+        .pipe(dest('dist'));
 }
 
 function minifyHTML() {
@@ -27,6 +29,14 @@ function minifyHTML() {
         .pipe(dest('dist'));
 }
 
+function addHashFileName() {
+    version =  (Math.random() + 1).toString(36).substring(7);
+
+    return src('dist/index.html')
+        .pipe(replace(/(\.(css)|(mjs))/g, '$1?_=' + version))
+        .pipe(dest('dist'));
+}
+
 module.exports = {
-    build: series(clearDist, convertSass, minifyHTML, clearTmp)
+    build: series(clearDist, parallel(convertSass, minifyMjs), minifyHTML, addHashFileName)
 }
--- src/scripts/main.mjs
+++ src/scripts/main.mjs
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";
-import { getFirestore, collection, query } from 'https://www.gstatic.com/firebasejs/9.2.0/firebase-firestore.js'
+import { getFirestore, collection, query } from 'https://www.gstatic.com/firebasejs/9.2.0/firebase-firestore.js';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
