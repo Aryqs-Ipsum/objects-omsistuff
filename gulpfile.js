7f528389960535ed0ea33470719d1783ad89f018
Arno Cellarier <mrfeyurikenpro@gmail.com>
2021-10-31 10:18:19 +0100

    switch to parcel

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,5 @@
-const {src, dest, watch, series, parallel} = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
-const htmlmin = require('gulp-htmlmin');
-const inlinesource = require('gulp-inline-source-html');
-const del = require('del');
-const terser = require('gulp-terser');
-const replace = require('gulp-replace');
-const include = require('gulp-html-tag-include');
+const {src, dest} = require("gulp");
 const gulpMinifyCssNames = require('gulp-minify-css-names');
-
-function clearDist() {
-    return del('dist');
-}
-
-function convertSass() {
-    return src('src/styles/*.scss')
-        .pipe(sass())
-        .pipe(dest('dist'));
-}
-
-function minifyMjs() {
-    return src('src/scripts/*.mjs')
-        .pipe(terser())
-        .pipe(dest('dist'));
-}
-
-function minifyHTML() {
-    return src('src/index.html')
-        .pipe(include())
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(inlinesource({ compress: true }))
-        .pipe(dest('dist'));
-}
 
 function minifyClassNames() {
     return src(['dist/index.html', 'dist/*.css'])
@@ -41,21 +10,6 @@ function minifyClassNames() {
         .pipe(dest('dist'))
 }
 
-function addHashFileName() {
-    version =  (Math.random() + 1).toString(36).substring(7);
-
-    return src('dist/index.html')
-        .pipe(replace(/(\.(css)|(mjs))/g, '$1?_=' + version))
-        .pipe(dest('dist'));
-}
-
-function watcher() {
-    watch('src/**/*', series(parallel(convertSass, minifyMjs), minifyHTML));
-}
-
-const build = series(clearDist, parallel(convertSass, minifyMjs), minifyHTML, minifyClassNames, addHashFileName);
-
 module.exports = {
-    build: build,
-    watch: watcher
+    build: minifyClassNames
 }
---
+++ src/scripts/main.js
@@ -0,0 +1,88 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, query, getDocs, where } from "firebase/firestore";
+import Alpine from "alpinejs";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC1RwlkVE5Fk1IzuENTK2lk2MXaumBRZmc",
+    authDomain: "objects-omsistuff.firebaseapp.com",
+    databaseURL: "https://objects-omsistuff.firebaseio.com",
+    projectId: "objects-omsistuff",
+    storageBucket: "objects-omsistuff.appspot.com",
+    messagingSenderId: "798024256030",
+    appId: "1:798024256030:web:1be5e23fc368de067b3c0f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+Alpine.data('app', () => ({
+    report: '',
+    objects: [],
+
+    isValidReport() {
+        return this.report.length > 0 && this.report.match(/Sceneryobjects\\.+\\.+\.sco/g)
+    },
+
+    getColorFromName(name) {
+        const nbColors = 3;
+        const base = '--color-';
+
+        // recover number between 1 and nbColors
+        let colorNumber = name.length
+        while (colorNumber > nbColors) {
+            colorNumber = colorNumber - nbColors;
+        }
+
+        return `${base}${colorNumber}`;
+    },
+
+    paths() {
+        let buffer = [];
+        this.report.split('\n').forEach(line => {
+            if (line.match(/Sceneryobjects\\.+\\.+\.sco/g)) {
+                const text = line.replace(/Sceneryobjects\\(.+)\\.+\.sco/g, '$1');
+                if(!buffer.includes(text))
+                    buffer.push(text);
+            }
+        });
+        return buffer;
+    },
+
+    pasted() {
+        setTimeout(() => { 
+            if(this.isValidReport()) {
+                this.fetchDb();
+                document.querySelector('#step-3').scrollIntoView();
+            }
+        }, 250);
+    },
+
+    async fetchDb() {
+        this.objects = [];
+
+        // check if query has less than 10 paths
+        const paths = this.paths();
+
+        // split paths in chunks of 10
+        const chunks = [];
+        for (let i = 0; i < paths.length; i += 10) {
+            chunks.push(paths.slice(i, i + 10));
+        }
+
+        // fetch all chunks
+        chunks.forEach(async chunk => {
+            const q = query(collection(db, "objects"), where('path', 'in', chunk));
+
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                this.objects.push({id: doc.id, ...doc.data()});
+            });
+        });
+    }
+}));
+
+Alpine.start();
